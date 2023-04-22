b8d878b328ad46a96a690acdcaf26a65b063b2e5
kubat <kubat.aiupov@gmail.com>
2023-04-22 07:55:25 +0200

    add truncateString

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -1,13 +1,13 @@
-// Repeat a given string str (first argument) for num times (second argument). 
-// Return an empty string if num is not a positive number. For the purpose of 
+// Repeat a given string str (first argument) for num times (second argument).
+// Return an empty string if num is not a positive number. For the purpose of
 // this challenge, do not use the built-in .repeat() method.
 
 const repeatStringNumTimes = (str, num) => {
-  let strToRepeat = '';
-  for(let i = 0; i < num; i++) {
+  let strToRepeat = "";
+  for (let i = 0; i < num; i++) {
     strToRepeat += str;
   }
-  return strToRepeat
-}
+  return strToRepeat;
+};
 
-console.log(repeatStringNumTimes('abc', 3))
+console.log(repeatStringNumTimes("abc", 3));
